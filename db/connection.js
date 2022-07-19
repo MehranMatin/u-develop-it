b33344a6048423b2b07d3525c0f6b55fa4e7ff87
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-07-19 19:20:04 -0400

    modules

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection({
     // Your MySQL username
     user: 'root',
     // Your MySQL password
-    password: '76498513',
+    password: '18001800',
     database: 'election'
 });
 
--- server.js
+++ server.js
@@ -1,74 +1,25 @@
-const db = require('./db/connection');
 // import express
 const express = require('express');
+// database
+const db = require('./db/connection');
 // import module given to us by colleage
 const inputCheck = require('./utils/inputCheck');
+// can omit file name from path because index.js is default
+const apiRoutes = require('./routes/apiRoutes');
+
 // port designation
 const PORT = process.env.PORT || 3001;
 // expression to instantiate express
 const app = express();
-// can omit file name from path because index.js is default
-const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Get all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// Get a single party
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-// Delete a party
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: res.message });
-            // checks if anything was deleted
-        } else if (!result.affectedRows) {
-            res.json({
-                message: 'Party not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
